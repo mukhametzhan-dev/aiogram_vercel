cefa1b5d0d5221b3db2abb0edf32cbd31a83656a
Mukhamedzhan <128323894+mukhametzhan-dev@users.noreply.github.com>
2025-01-20 02:29:49 +0600

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,24 +1,30 @@
 import logging
 from urllib.parse import urljoin
+import asyncio
 
-from aiogram import Bot, Dispatcher, executor
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.types import BotCommand
+from aiogram.utils.chat_action import ChatActionMiddleware
 
 from config import Config
 
+# Constants for Webhook configuration
 WEBHOOK_PATH = f"/api/bot/{Config.BOT_TOKEN}"
 WEBHOOK_URL = urljoin(Config.WEBHOOK_HOST, WEBHOOK_PATH)
 ALLOWED_UPDATES = ["message", "callback_query", "inline_query"]
 
-
+# Logging configuration
 logging.basicConfig(
     level=logging.INFO,
     datefmt="%Y/%m/%d %H:%M:%S",
     format="[%(asctime)s][%(name)s][%(levelname)s] ==> %(message)s",
 )
 
-client = Bot(token=Config.BOT_TOKEN)
+# Bot and Dispatcher initialization
+bot = Bot(token=Config.BOT_TOKEN)
 storage = MemoryStorage()
-dp = Dispatcher(client, storage=storage)
-dp.middleware.setup(LoggingMiddleware())
+dp = Dispatcher(storage=storage)
+
+# Middleware setup
+dp.message.middleware(ChatActionMiddleware())  # Adds typing and other chat actions
